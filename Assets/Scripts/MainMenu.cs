6b693ce52ab7a517d1899cf90e23db8abadf8b3b
Calvin Chu <calvinchd@gmail.com>
2018-08-15 22:23:50 +1000

    Created main menu scene

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("TestLevel");
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
