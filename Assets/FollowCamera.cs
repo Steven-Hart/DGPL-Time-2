22bc6f46ddbae4861ec01ba6fa73e6b1fa94583b
Unknown <trent.braden.williams@gmail.com>
2018-09-26 21:21:56 +1000

    Level, lighting, gradient, vignette and post-processing added in test files

---
+++ Assets/FollowCamera.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class FollowCamera : MonoBehaviour {
+
+    private Camera cam;
+    public Vector3 offset;
+
+	// Use this for initialization
+	void Start () {
+        cam = Camera.main;
+        transform.rotation = cam.transform.rotation;
+        transform.position = cam.transform.position - offset;
+	}
+}
