d347f4b64c579f9d88f68cdbe873397ca0758d70
Unknown <trent.braden.williams@gmail.com>
2018-09-30 16:57:58 +1000

    Added switches, gates, texture edits and project file management

---
+++ Assets/GetColour.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class GetColour : MonoBehaviour {
+
+    public GameObject button;
+    private Color buttonColour;
+
+	// Use this for initialization
+	void Start () {
+	}
+
+    private void Update()
+    {
+        buttonColour = button.GetComponent<Renderer>().sharedMaterial.color;
+        GetComponent<SpriteRenderer>().color = buttonColour;
+    }
+}
---
+++ Assets/Rotate.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class Rotate : MonoBehaviour {
+
+    private float speed;
+
+    private void Start()
+    {
+        speed = 100f;
+    }
+
+    // Update is called once per frame
+    void Update () {
+        transform.Rotate(Vector3.left, Time.deltaTime * speed);
+	}
+}
