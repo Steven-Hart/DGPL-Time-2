4891414050d62014de95b0a1e2804978453cd3c8
Gunto <trent.braden.williams@gmail.com>
2018-10-03 20:48:35 +1000

    Changed materials to allow fading and added particle render texture

---
+++ Assets/Scripts/FadeFix.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class FadeFix : MonoBehaviour {
+
+    private Material m;
+
+	// Use this for initialization
+	void Start () {
+        m = GetComponent<Renderer>().sharedMaterial;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        m.SetInt("_ZWrite", 1);
+	}
+}
