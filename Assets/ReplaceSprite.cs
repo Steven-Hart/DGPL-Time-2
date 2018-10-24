c07f9099cecb234c3060fc2441babd029b940553
Gunto <trent.braden.williams@gmail.com>
2018-10-24 21:40:21 +1100

    Prefeb meta files, added items for moves and lives UI, script to spawn highlight for tile

---
+++ Assets/ReplaceSprite.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class ReplaceSprite : MonoBehaviour {
+
+    private SpriteRenderer[] sprites;
+    public Sprite sprite;
+    public Color colour;
+
+	// Use this for initialization
+	void Start () {
+        sprites = GetComponentsInChildren<SpriteRenderer>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		foreach (SpriteRenderer s in sprites)
+        {
+            s.sprite = sprite;
+            s.color = colour;
+        }
+	}
+}
---
+++ Assets/Scripts/LightTile.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightTile : MonoBehaviour {
+
+    public GameObject tileHighlight;
+    public Transform parent;
+
+    public void Highlight(Vector3 playerLocation)
+    {
+        Vector3 position = new Vector3(playerLocation.x, tileHighlight.transform.position.y, playerLocation.z);
+        GameObject highlight = Instantiate(tileHighlight, position, tileHighlight.transform.rotation, parent);
+    }
+}
