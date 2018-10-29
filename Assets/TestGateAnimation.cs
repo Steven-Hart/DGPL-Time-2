36fa4b6263237623c01acbdf9e73d486a2f4dc5a
Gunto <trent.braden.williams@gmail.com>
2018-10-29 21:07:26 +1100

    Testing animations, made changes to some prefabs, new animation clips

---
+++ Assets/TestGateAnimation.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestGateAnimation : MonoBehaviour {
+
+    public Animator[] anims;
+    
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.Space))
+        {
+            anims[0].Play("Disable Gate");
+            anims[2].Play("Fade In");
+        }
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            anims[1].Play("Disable Gate");
+        }
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            anims[2].Play("Fade In");
+        }
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            anims[2].Play("Fade Out");
+        }
+    }
+}
